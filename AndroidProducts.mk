8978c76d768910785622492bbbc5cb12095efba8
dread <dreadcreator5@gmail.com>
2023-07-18 19:34:19 +0530

    misaka: prepare for lineage
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_misaka.mk
+	$(LOCAL_DIR)/lineage_misaka.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_misaka-user \
-    lineage_misaka-userdebug
+    $(foreach variant, user userdebug eng, lineage_misaka-$(variant))
