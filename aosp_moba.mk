c341ab4928b1bac3569606374290ffddc9daf261
isaquetp <superboydbr@gmail.com>
2023-07-24 12:43:05 -0300

    moba: add missing things and organise
    Signed-off-by: isaquetp <superboydbr@gmail.com>

--- aosp_moba.mk
+++ aosp_moba.mk
@@ -1,3 +1,27 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Common AOSP base
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device tree
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Common PE configs
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifiers.
@@ -10,8 +34,11 @@ PRODUCT_MODEL := L79031
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_moba.mk
 
-# Inherit from moba device
-$(call inherit-product, device/lenovo/moba/device.mk)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="moba-user 12 SKQ1.220119.001 13.5.351_221103 release-keys" \
+
+BUILD_FINGERPRINT := Lenovo/moba/moba:12/SKQ1.220119.001/13.5.351_221103:user/release-keys
 
 $(call inherit-product-if-exists, vendor/lenovo/moba/moba-vendor.mk)
- 
+
+
