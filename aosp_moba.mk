6b2f4a6523e9e929bc253b1f856c5633f00fb879
isaquetp <superboydbr@gmail.com>
2023-07-24 12:35:04 -0300

    moba: aosp bringup
    Signed-off-by: isaquetp <superboydbr@gmail.com>

--- aosp_moba.mk
+++ aosp_moba.mk
@@ -1,14 +1,14 @@
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifiers.
-PRODUCT_NAME := lineage_moba
+PRODUCT_NAME := aosp_moba
 PRODUCT_DEVICE := moba
 PRODUCT_MANUFACTURER := lenovo
 PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := L79031
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_moba.mk
+    $(LOCAL_DIR)/aosp_moba.mk
 
 # Inherit from moba device
 $(call inherit-product, device/lenovo/moba/device.mk)
